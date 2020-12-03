15b15ef5eacf0a78591be14fc82e39c45f889351
Loic TRUCHOT <loic.truchot.pro@gmail.com>
2020-12-03 10:51:04 +0100

    feat(main): static files

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 import './styles.scss';
-import kitten from './static/images/kitten.jpg';
+
+// eslint-disable-next-line import/no-unresolved
+import images from './static/images/*.*';
 
 // import $ from 'jquery';
 // import 'bootstrap';
 // $('body').append('jquery + bootstrap works!');
-
-document.body.innerHTML += `<img src="${kitten}" width="100"/>`;
+const app = document.getElementById('app');
+app.innerHTML += `<img src="${images.kitten.jpg}" style="width:100px;" />`;
+// or
+app.innerHTML += '<img src="images/kitten.jpg" style="width:100px;" />';
